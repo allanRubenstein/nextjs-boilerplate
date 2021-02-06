f1f9a3dd0775a9c7521c8b8d59ef5d5987267782
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-02-06 13:27:11 -0500

    adding styled-components LinkButton

---
+++ components/link-button/LinkButton.tsx
@@ -0,0 +1,36 @@
+import styled, {css} from 'styled-components';
+import { LinkButtonProps } from './types';
+import Link from 'next/link';
+
+const LinkButton: React.FunctionComponent<LinkButtonProps> = (
+  props: LinkButtonProps
+) => {
+
+  // if href is passed, use Link, else button
+  if (props.href) {
+    return (
+      <Link href={props.href} passHref>
+        <LinkWrapper>{props.children}</LinkWrapper>
+      </Link>
+    );
+  } else {
+    return <ButtonWrapper onClick={props.onClick}>{props.children}</ButtonWrapper>;
+  }
+}
+export default LinkButton;
+
+// shared css for button and link
+const LinkAndButtonCss = css`
+  /* Base styles */
+  color: red;
+`;
+
+const LinkWrapper =
+  styled.a`
+  ${LinkAndButtonCss}
+  /* link specific styles */
+`;
+const ButtonWrapper = styled.button`
+  ${LinkAndButtonCss}
+  /* button specific styles */
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Button from '../components/button/Button';
+import Button from '../components/link-button/LinkButton';
 
 export default function Home() {
   return (
