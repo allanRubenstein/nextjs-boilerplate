37b592442aac9120604b55a95b5bf9de7041af11
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-02-10 10:45:39 -0500

    more testing setup/config

--- components/link-button/__tests__/LinkButton.tsx
+++ components/link-button/__tests__/LinkButton.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
 import LinkButton from '../LinkButton';
+import { render } from '@testing-library/react';
 
 test('works', () => {
   // it works :)
+  const { debug } = render(<LinkButton>test</LinkButton>);
+
+  debug();
 });
