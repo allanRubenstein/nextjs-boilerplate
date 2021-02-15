71b1ae5f25724242c10ddc9856858f318d8aeb7d
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-02-15 10:26:00 -0500

    adding jest-styled-components

--- components/link-button/__tests__/LinkButton.tsx
+++ components/link-button/__tests__/LinkButton.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import LinkButton from '../LinkButton';
 import { render } from '@testing-library/react';
+import 'jest-styled-components';
 
 test('works', () => {
   // it works :)
-  const { debug } = render(<LinkButton>test</LinkButton>);
+  const { container } = render(<LinkButton>test</LinkButton>);
 
-  debug();
+  expect(container).toMatchInlineSnapshot(`
+    .c0 {
+      color: red;
+    }
+
+    <div>
+      <button
+        class="c0"
+      >
+        test
+      </button>
+    </div>
+  `);
 });
