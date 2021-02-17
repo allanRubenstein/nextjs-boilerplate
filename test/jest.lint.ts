181ab71127002b3a1258df3a355b375b7e4f2115
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-02-17 14:14:37 -0500

    not showing skipped files

--- test/jest.lint.ts
+++ test/jest.lint.ts
@@ -5,6 +5,11 @@ module.exports = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
   // this checks all files but leads to a bunch being skipped thanks to the "ignorePath": "./.gitignore" in package.json
-  // had to add the !** to fix some of them, TODO: find out which ones are skipped and why they still display
-  testMatch: ['<rootDir>/**/*.{tsx,ts,js}', '!**/.next/**/*'],
+  // had to add the !** directories/files to prevent "X Skipped" messaging when running jest lint
+  testMatch: [
+    '<rootDir>/**/*.{tsx,ts,js}',
+    '!**/.next/**/*',
+    '!**/_document.js',
+    '!**/coverage/**/*',
+  ],
 };
