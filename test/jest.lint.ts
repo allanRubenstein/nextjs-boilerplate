e22e557ce19723d804a8115f887df734fc58151d
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-02-17 13:16:30 -0500

    linting js and ts/tsx files at the same time

--- test/jest.lint.ts
+++ test/jest.lint.ts
@@ -4,5 +4,7 @@ module.exports = {
   rootDir: path.join(__dirname, '..'),
   displayName: 'lint',
   runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/**/*.{tsx,ts,js}'],
+  // this checks all files but leads to a bunch being skipped thanks to the "ignorePath": "./.gitignore" in package.json
+  // had to add the !** to fix some of them, TODO: find out which ones are skipped and why they still display
+  testMatch: ['<rootDir>/**/*.{tsx,ts,js}', '!**/.next/**/*'],
 };
